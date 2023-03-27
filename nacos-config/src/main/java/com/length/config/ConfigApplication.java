c0cda9b758cb671099c0cd1f707093996a8e16af
翟皓 <2064866281@qq.com>
2023-03-27 18:02:09 +0800

    loardblancer的体验

--- nacos-config/src/main/java/com/length/config/ConfigApplication.java
+++ nacos-config/src/main/java/com/length/config/ConfigApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class ConfigApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext applicationContext= SpringApplication.run(ConfigApplication.class, args);
-        String userName =applicationContext.getEnvironment().getProperty("user.name");
-        String userAge =applicationContext.getEnvironment().getProperty("user.age");
-        System.out.println("user name: "+userName+" user age: "+userAge);
+        while(true) {
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            String config = applicationContext.getEnvironment().getProperty("user.config");
+            String area = applicationContext.getEnvironment().getProperty("user.area");
+            System.out.println("user name: " + userName + " user age: " + userAge+" user config: "+config);
+            System.out.println("user area:  "+area);
+            TimeUnit.SECONDS.sleep(2);
+        }
     }
 }
